1745cb8a809eaf1d4b56ac4e06a1c31a0048b378
HoussemPRO <47675752+HoussemPRO@users.noreply.github.com>
2020-06-18 12:01:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');  //WESO#0001
 
-var prefix = "$";
-let Cooldown = new Set()
-let cdsc = 60000;
 
 
 client.on('ready', () => {
